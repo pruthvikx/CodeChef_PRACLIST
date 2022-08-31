5a90e7c6efb4021300fefc453eab0f379350b227
Pruthvi <pruthvikx121@gmail.com>
2022-08-31 16:54:50 +0530

    Codechef praclist challenge

---
+++ src/com/company/Problem.java
@@ -0,0 +1,19 @@
+/*
+
+Our Chef is currently practicing on CodeChef and is a beginner.
+The count of ‘All Problems’ in the Beginner section is XX.
+Our Chef has already ‘Attempted’ YY problems among them.
+How many problems are yet ‘Un-attempted’?
+
+Input Format
+
+The first and only line of input contains two space-separated integers
+XX and YY — the count of 'All problems' in the Beginner's
+section and the count of Chef's 'Attempted' problems, respectively.
+
+Output Format
+
+Output a single integer in a single line — the number of
+problems that are yet 'Un-attempted'
+
+ */
